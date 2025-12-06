4df0ffbaf6e4564f8ae0cf1abc4d8a6b0e1c8d10
Jair Cordero <cordero1823.cr@gmail.com>
2025-12-06 11:25:22 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola mundo</h1>
+      <div>
+        <h2>Hola</h2>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
